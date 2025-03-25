17794b1497310ab2969b6c41aec9002b8c85029a
yar <yar@gmail.com>
2025-03-25 23:47:47 +0200

    Initial commit

---
+++ test/cdk_aws_cheat_sheet.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAwsCheatSheet from '../lib/cdk_aws_cheat_sheet-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_aws_cheat_sheet-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAwsCheatSheet.CdkAwsCheatSheetStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
